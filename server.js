4c111f4c97306b4056681133c8b32d509c88ea1e
techboy-s <65527961+techboy-s@users.noreply.github.com>
2023-04-18 23:16:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ app.listen(PORT, () => {
   // console.log(`Server running on ${PORT}`);
 });
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
 });
 
 module.exports = app;
